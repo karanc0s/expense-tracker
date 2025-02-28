d2c9f4151fafdb5c88e4b9e534d1c3e5d560e813
Karan <karancs.sat@gmail.com>
2025-03-01 04:15:46 +0530

    Kafka integrating AuthService is working , UserService have issues

---
+++ AuthService/src/main/java/com/karan/authservice/service/ProducerService.java
@@ -0,0 +1,50 @@
+package com.karan.authservice.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import com.karan.authservice.Dto.UserInfoDTO;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class ProducerService {
+
+
+    @Value(value = "${spring.kafka.topic.user-creation}")
+    private final String topicName;
+
+    private final KafkaTemplate<String, UserInfoDTO> producer;
+    
+    
+
+    public void sendUserEvent(UserInfoDTO userInfoDTO){
+    
+        try{
+            SendResult<String, UserInfoDTO> result = producer.send(
+                topicName,
+                userInfoDTO
+            ).get();
+
+            System.out.println("Sent message=[" + userInfoDTO + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        // ProducerRecord<String , UserInfoDTO> record = new ProducerRecord<>(
+        //     topicName,
+        //     "USER_EVENT_CREATED" ,
+        //     userInfoDTO
+        // );
+
+        // producer.send(record , (metadata , exception)->{
+        //     if (exception != null) {
+        //         System.err.println("Error sending message to Kafka: " + exception.getMessage());
+        //     } else {
+        //         System.out.println("Message sent to Kafka, offset: " + metadata.offset());
+        //     }
+        // });
+    }
+}
